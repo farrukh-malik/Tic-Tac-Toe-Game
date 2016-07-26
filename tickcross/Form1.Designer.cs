81f49806aed3f50e4521877b083de60a9b646791
farrukh <farrukhmalik46@gmail.com>
2016-07-26 01:16:32 -0700

    tick cross game

---
+++ tickcross/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace tickcross
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.p1Score = new System.Windows.Forms.Label();
+            this.p2Score = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 46);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 46);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 46);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(44, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 46);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(111, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 46);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(178, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 46);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(44, 170);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 46);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(111, 170);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 46);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(178, 170);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 46);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(31, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "player 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(150, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "player 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(102, 226);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "player 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "***SCORE***";
+            // 
+            // p1Score
+            // 
+            this.p1Score.AutoSize = true;
+            this.p1Score.Location = new System.Drawing.Point(60, 342);
+            this.p1Score.Name = "p1Score";
+            this.p1Score.Size = new System.Drawing.Size(0, 13);
+            this.p1Score.TabIndex = 17;
+            // 
+            // p2Score
+            // 
+            this.p2Score.AutoSize = true;
+            this.p2Score.Location = new System.Drawing.Point(229, 342);
+            this.p2Score.Name = "p2Score";
+            this.p2Score.Size = new System.Drawing.Size(0, 13);
+            this.p2Score.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "player 2:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 407);
+            this.Controls.Add(this.p2Score);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.p1Score);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label p1Score;
+        private System.Windows.Forms.Label p2Score;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ tickcross/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tickcross
+{
+    public partial class Form1 : Form
+    {
+        int[] s = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked){
+                button1.BackgroundImage = Properties.Resources.Untitled23;                
+                radioButton2.Checked = true;
+                s[1] =1;
+
+            } else if(radioButton2.Checked ){
+
+                button1.BackgroundImage = Properties.Resources.Untitled24;                
+                radioButton1.Checked = true;
+                s[1] = 2;
+            }
+
+            button1.Enabled = false; func();
+      
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button2.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[2] = 1;
+
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button2.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[2] = 2;
+            }
+
+            button2.Enabled = false; func();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button3.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[3] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button3.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[3] = 2;
+            }
+
+            button3.Enabled = false; func();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button6.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[6] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button6.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[6] = 2;
+            }
+
+            button6.Enabled = false; func();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button5.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[5] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button5.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[5] = 2;
+            }
+
+            button5.Enabled = false; func();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button4.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[4] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button4.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[4] = 2;
+            }
+
+            button4.Enabled = false; func();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button9.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[9] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button9.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[9] = 2;
+            }
+
+            button9.Enabled = false; func();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button8.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[8] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button8.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[8] = 2;
+            }
+
+            button8.Enabled = false; func();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button7.BackgroundImage = Properties.Resources.Untitled23;
+                radioButton2.Checked = true;
+                s[7] = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+
+                button7.BackgroundImage = Properties.Resources.Untitled24;
+                radioButton1.Checked = true;
+                s[7] = 2;
+            }
+
+            button7.Enabled = false;
+            func();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button1.BackgroundImage = null;
+
+
+            button2.Enabled = true;
+            button2.BackgroundImage = null;
+
+
+            button3.Enabled = true;
+            button3.BackgroundImage = null;
+
+
+            button4.Enabled = true;
+            button4.BackgroundImage = null;
+
+
+            button5.Enabled = true;
+            button5.BackgroundImage = null;
+
+
+            button6.Enabled = true;
+            button6.BackgroundImage = null;
+
+
+            button7.Enabled = true;
+            button7.BackgroundImage = null;
+
+
+            button8.Enabled = true;
+            button8.BackgroundImage = null;
+
+
+            button9.Enabled = true;
+            button9.BackgroundImage = null;                        
+        for(int i=0;i<10;i++){
+            s[i] = 0;
+        }
+        }
+
+        int p1 = 0, p2 = 0;
+
+        void func() { 
+
+            if(
+                s[1] == 1 && s[2] == 1 && s[3] == 1 ||
+                s[4] == 1 && s[5] == 1 && s[6] == 1 ||
+                s[7] == 1 && s[8] == 1 && s[9] == 1 ||
+
+                s[1] == 1 && s[4] == 1 && s[7] == 1 ||
+                s[2] == 1 && s[5] == 1 && s[8] == 1 ||
+                s[3] == 1 && s[6] == 1 && s[9] == 1 ||
+
+                s[1] == 1 && s[5] == 1 && s[9] == 1 ||
+                s[3] == 1 && s[5] == 1 && s[7] == 1    ){
+                
+                p1++;
+                p1Score.Text = p1.ToString();
+                disableAll();
+
+            }else  if(
+                s[1] == 2 && s[2] == 2 && s[3] == 2 ||
+                s[4] == 2 && s[5] == 2 && s[6] == 2 ||
+                s[7] == 2 && s[8] == 2 && s[9] == 2 ||
+
+                s[1] == 2 && s[4] == 2 && s[7] == 2 ||
+                s[2] == 2 && s[5] == 2 && s[8] == 2 ||
+                s[3] == 2 && s[6] == 2 && s[9] == 2 ||
+
+                s[1] == 2 && s[5] == 2 && s[9] == 2 ||
+                s[3] == 2 && s[5] == 2 && s[7] == 2)
+            {
+
+                p2++;
+                p2Score.Text = p2.ToString();
+                disableAll();           
+            }
+            
+        }// func end here
+
+        void disableAll() {
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+
+
+}
